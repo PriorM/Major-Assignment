06d9e51341ecbf02754efc5e6cb0bf057f7f8df5
unknown <mjp151@uowmail.edu.au>
2019-05-06 15:03:12 +1000

    open database

--- Task 1/Main.php
+++ Task 1/Main.php
@@ -3,4 +3,10 @@
     </head>
     <body>
     </body>
+    <?php
+        //Opens database connection and starts session
+        $db = mysql_connect('localhost', 'root', '', 'task1') or die('DB Failure: '.mysql_error());
+        session_start();
+        $sid = session_id();
+    ?>
 </html>
