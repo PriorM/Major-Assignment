7a2070c54e68947a8b7c0ac41a9c24886a3ae0cb
unknown <mjp151@uowmail.edu.au>
2019-05-20 18:50:07 +1000

    Task 6 completed

--- Task 6/PartC.php
+++ Task 6/PartC.php
@@ -1,122 +1,130 @@
 <?php
-    class Binary_Tree_Node
-    {
+
+    class Binary_Tree_Node {
+
         public $data;
+
         public $left;
         public $right;
 
-        public function _construct($d = NULL)
+        public function __construct($d = NULL) 
         {
             $this->data = $d;
         }
 
-        public function traversePreorder()
+        public function traversePreorder() 
         {
+
             $l = array();
             $r = array();
 
-            if ($this->left)
-            {
-                $l = $this->left->traversePreorder();
+            if ($this->left) 
+            { 
+                $l = $this->left->traversePreorder(); 
             }
-            if ($this->right)
-            {
-                $r = $this->right->traversePreorder();
+            if ($this->right) 
+            { 
+                $r = $this->right->traversePreorder(); 
             }
 
             return array_merge(array($this->data), $l, $r);
         }
 
-        public function traversePostorder()
+        public function traversePostorder() 
         {
+
             $l = array();
             $r = array();
 
-            if ($this->left)
-            {
-                $l = $this->left->traversePostorder();
+            if ($this->left) 
+            { 
+                $l = $this->left->traversePostorder(); 
             }
-            if ($this->right)
-            {
-                $r = $this->right->traversePostorder();
+            if ($this->right) 
+            { 
+                $r = $this->right->traversePostorder(); 
             }
 
             return array_merge($l, $r, array($this->data));
         }
 
-        public function traverseInorder()
+        public function traverseInorder() 
         {
             $l = array();
             $r = array();
 
-            if ($this->left)
+            if ($this->left) 
             {
-                $l = $this->left->traverseInorder();
+                 $l = $this->left->traverseInorder(); 
             }
-            if ($this->right)
+            if ($this->right) 
             {
-                $r = $this->right->traverseInorder();
+                 $r = $this->right->traverseInorder(); 
             }
 
             return array_merge($r, array($this->data), $l);
         }
     }
-    class Sorting_Tree
+
+    class Sorting_Tree 
     {
         public $tree;
 
-        public function insert($val)
+        public function insert($val) 
         {
-            if (!(isset($this->tree)))
+            if (!(isset($this->tree))) 
             {
                 $this->tree = new Binary_Tree_Node($val);
-            }
-            else
+            } 
+            else 
             {
                 $pointer = $this->tree;
-                for (; ; )
+                for(;;) 
                 {
-                    if ($val <= $pointer->data)
+                    if ($val <= $pointer->data) 
                     {
-                        if ($pointer->left)
+                        if ($pointer->left) 
                         {
                             $pointer = $pointer->left;
-                        }
-                        else
+                        } 
+                        else 
                         {
-                            $pointer->left = new Binary_Tree_node($val);
+                            $pointer->left = new Binary_Tree_Node($val);
                             break;
                         }
                     }
                     else
                     {
-                        if ($pointer->right)
+                        if ($pointer->right) 
                         {
                             $pointer = $pointer->right;
-                        }
-                        else
+                        } 
+                        else 
                         {
-                            $pointer->right = new  Binary_Tree_Node($val);
+                            $pointer->right = new Binary_Tree_Node($val);
                             break;
                         }
                     }
                 }
             }
-        }
-
-        public function returnSorted()
+        }   
+        public function returnSorted() 
         {
             return $this->tree->traverseInorder();
         }
     }
 
-    $nums = array(69, 87, 65, 56, 37, 73, 50, 81, 23, 95, 53, 86, 57, 16, 35, 1, 55, 70, 26, 21);
-    $sort_as_you_go = new Sorting_Tree();
+    $sort_as_you_go1 = new Sorting_Tree();
+    $sort_as_you_go2 = new Sorting_Tree();
+    $sort_as_you_go3 = new SOrting_Tree();
 
-    for ($i = 0; $i < 20; $i++)
+    for ($i = 0; $i < 20; $i++) 
     {
-        $sort_as_you_go->insert($nums[$i]);
+        $sort_as_you_go1->insert(rand(1,100));
+        $sort_as_you_go2->insert(rand(1,100));
+        $sort_as_you_go3->insert(rand(1,100));
     }
-
-    echo "<p>", implode(', ', $sort_as_you_go->returnSorted()), "</p>"; 
+    echo "<p> Data Set 1: ", implode(", ", $sort_as_you_go1->returnSorted()), "</p>";
+    echo "<p> Data Set 2: ", implode(", ", $sort_as_you_go2->returnSorted()), "</p>";
+    echo "<p> Data Set 3: ", implode(", ", $sort_as_you_go3->returnSorted()), "</p>";
 ?>
